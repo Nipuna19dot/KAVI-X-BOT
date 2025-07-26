6e1167760d98cee0011d3b3c104dcb4edcec7144
Nipunayt <nipunanamal698@gmail.com>
2025-07-26 13:41:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 global.sessionID = process.env.SESSIONID || "ᴘᴜᴛ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ʜᴇʀᴇ."
-global.botname = process.env.BOTNAME || "KAVI-X MD"
+global.botname = process.env.BOTNAME || "NIPUNA-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94702128378"
 global.ownername = process.env.OWNERNAME || "Cyber Kavi"
 global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
@@ -10,7 +10,7 @@ global.wagc = process.env.WAGC || "https://chat.whatsapp.com/CG9f0paHJzwDImXiydf
 global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/KAVI-X-BOT"
 global.packname = process.env.PACKNAME || "KAVI-X MD"
 global.author = process.env.AUTHOR || "Cyber Kavi"
-global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
+global.creator = process.env.CREATOR || "94 772915479@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
 global.mongoDB = process.env.MONGODB_URI || "ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ᴜʀɪ ʜᴇʀᴇ."
